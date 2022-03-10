95126f8a44e272297fa777cae1e9cde0655f237d
PranavRudra99 <pranavrudra1999@gmail.com>
2022-03-10 08:21:45 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Customers from './Customers'
+import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import CustomerDetails from './CustomerDetails';
+
+
+class App extends Component {
+  render() {
+    console.log("Host URL"+process.env.PUBLIC_URL);
+    return (
+
+      <Router basename={process.env.PUBLIC_URL}>
+        <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">React</h1>
+        </header>
+          <Switch>
+                <Route exact path= "/" render={() => (
+                  <Redirect to="/customerlist"/>
+                )}/>
+                 <Route exact path='/customerlist' component={Customers} />
+                 <Route exact path='/customerdetail/:id' component={CustomerDetails} />
+          </Switch>
+      </div>
+    </Router>
+    );
+  }
+}
+
+export default App;

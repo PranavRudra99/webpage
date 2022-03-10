cb49c76982d91985afb7457f5cdffde580e64c74
PranavRudra99 <pranavrudra1999@gmail.com>
2022-03-10 12:16:22 -0500

    working on 2b

--- src/CustomerDetails.js
+++ src/CustomerDetails.js
@@ -15,6 +15,7 @@ export default class CustomerDetails extends Component {
   //Function which is called when the component loads for the first time
   componentDidMount() {
     let id = this.props.match.params.id;
+    this.state.userId = id;
     // let { id } = useParams();//this.props.route.params;
     
     
@@ -35,18 +36,25 @@ export default class CustomerDetails extends Component {
   getAlbums(id) {
     axios.get('https://jsonplaceholder.typicode.com/albums?userId=' + id).then(response => {
       this.setState({customerAlbums: response})
+      let flags = [];
+      for(let i =0; i < response.data.length; i++){
+        flags[i]=false;
+      }
+      this.setState({showAlbumImages: flags})
     })
   };
 
   //Function to Load the customer posts data from json.
   getPosts(id) {
     axios.get('https://jsonplaceholder.typicode.com/posts?userId=' + id).then(response => {
-      this.setState({customerPosts: response})
+      this.setState({customerPosts: response.data})
       let flags = [];
+      let comments = [];
       for(let i =0; i < response.data.length; i++){
         flags[i]=false;
+        comments[i] = {};
       }
-    this.setState({showAlbumImages: flags})
+      this.setState({showPostComments: flags, postComments: comments})
     });
   };
 
@@ -65,7 +73,33 @@ export default class CustomerDetails extends Component {
       arr[index] = !this.state.showAlbumImages[index]
       this.setState({showAlbumImages: [...arr]})
     })
-  } 
+  }
+
+  showComments(postId,index){
+    axios.get('https://jsonplaceholder.typicode.com/comments?postId=' + postId).then(response => {
+        // this.setState({postComments:response})
+        let arr1 = [...this.state.showPostComments]
+        arr1[index] = !this.state.showPostComments[index]
+        let arr2 = [... this.state.postComments]
+        arr2[index] = response;
+        this.setState({showPostComments:arr1, postComments:arr2})
+    });
+  }
+
+  deletePost(postId, index){
+    axios.delete('https://jsonplaceholder.typicode.com/posts/' + postId).then(response => {
+      if(response.status === 200){
+        let arr1 = [...this.state.customerPosts];
+        arr1.splice(index, 1);
+        let arr2 = [...this.state.showPostComments];
+        arr2.splice(index, 1);
+        let arr3 = [...this.state.postComments];
+        console.log(this.state.postComments)
+        this.setState({customerPosts: [...arr1], showPostComments: [...arr2], postComments: [...arr3]})
+        // console.log(this.state.customerPosts)
+      }
+    });
+  }
 
 
 
@@ -97,8 +131,25 @@ export default class CustomerDetails extends Component {
         </Panel.Heading>
         {this.state.showPost == true ?<Panel.Body>
           {this.state.customerPosts? 
-            this.state.customerPosts.data.map(post => 
-              <div key={post.id} onClick={() => {}}>{post.title}</div>)
+            this.state.customerPosts.map((post,index) =>
+              <div key={post.id}>
+                  <div>
+                    <Panel.Body onClick={() => {this.showComments(post.id,index)}}>{post.title}</Panel.Body>
+                    {/* <button className='btn btn-primary' onClick={() => {this.updatePost(post.id)}}>Update Post</button> */}
+                    <button className='btn btn-primary' onClick={() => {this.deletePost(post.id, index)}}>Delete Post</button>
+                  </div>
+                  
+                  <Panel bsStyle="info" className="centeralign">
+                  {this.state.showPostComments[index] === true ?<Panel.Body>
+                  {this.state.postComments[index]?
+                  this.state.postComments[index].data.map(comment =>
+                  <div key={comment.id}>
+                    {comment.name}
+                  </div>)
+                  : ''}
+                  </Panel.Body>: ''}
+                </Panel>
+              </div>)
           : ''}
         </Panel.Body>: ''}
       </Panel>
@@ -128,16 +179,6 @@ export default class CustomerDetails extends Component {
         </Panel.Body>: ''}
       </Panel>
 
-      {/* <Panel bsStyle="info" className="centeralign">
-        {this.state.showAlbumImages == true ?<Panel.Body>
-          {this.state.albumImages?
-            this.state.albumImages.data.map(albumImage =>
-              <div key={albumImage.id} className="flex-row">
-                  <img src={albumImage.thumbnailUrl} alt="Image"/>
-              </div>)
-          : ''}
-        </Panel.Body>: ''}
-      </Panel> */}
     </div>)
   }
 }
